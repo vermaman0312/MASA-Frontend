0f0a74deaf741ff0748aaf877c2b314027758956
Aman <aman.verma@thegatewaycorp.co.in>
2024-08-08 18:15:35 +0530

    updated code ...

--- src/layouts/private-layouts/private-profile-layout/page.layout.tsx
+++ src/layouts/private-layouts/private-profile-layout/page.layout.tsx
@@ -1,11 +1,21 @@
+import { useSelector } from "react-redux";
+import { CustomInputField } from "../../../components/custom-input-field/custom-input-field.component";
 import { CustomLabel } from "../../../components/custom-label/custom-label.component";
 import CustomSignatureName from "../../../components/custom-signature-name/custom-signature-name.component";
+import { RootState } from "../../../redux/redux-index";
+import { profileDetailsResponseInterface } from "../../../api/models/private-api-models/private-profile-details-api.model";
 
 const PrivateProfilePageLayout = () => {
+  const profileDetails = useSelector(
+    (state: RootState) => state.privateProfileState.profileDetails
+  );
+  const profileData = (profileDetails as profileDetailsResponseInterface).data
+    ?.Data;
+
   return (
     <div className="w-full h-full">
       <div className="w-full h-full flex flex-col md:flex-row items-center justify-between gap-1">
-        <div className="h-full w-full md:w-[30rem]">
+        <div className="h-full w-full md:w-[30rem] flex flex-col">
           <div className="w-full h-[10rem] p-1">
             <div className="w-full h-full bg-white rounded-xl shadow-md shadow-gray-500">
               <CustomSignatureName userName="Aman Verma" />
@@ -13,14 +23,146 @@ const PrivateProfilePageLayout = () => {
             <div className="w-[5rem] h-[5rem] bg-gray-200 rounded-full relative -top-8 left-[5%] shadow-md shadow-gray-500 z-50">
               <img
                 className="w-full h-full rounded-full"
-                src="https://randomuser.me/api/portraits/men/1.jpg"
+                src={
+                  profileData?.userProfileImage ??
+                  "https://randomuser.me/api/portraits/men/1.jpg"
+                }
                 alt=""
               />
             </div>
-            <div className="w-full relative -top-16 flex items-center justify-end">
+            <div className="w-full relative -top-16 flex flex-col items-end justify-end">
               <CustomLabel className="font-display text-lg text-gray-500">
-                Aman Verma
+                {`${profileData?.userFirstName} ${profileData?.userLastName}`}
               </CustomLabel>
+              <CustomLabel className="font-display text-xs text-gray-400">
+                {profileData?.userRole}
+              </CustomLabel>
+            </div>
+          </div>
+          <div className="w-full h-full flex flex-col items-start justify-start mt-16">
+            <div className="w-full">
+              <CustomLabel
+                className="font-medium text-xs font-display text-[#0d1b2a]"
+                htmlFor="email"
+              >
+                Email address
+              </CustomLabel>
+              <CustomInputField
+                disabled
+                id="email"
+                type="text"
+                value={profileData?.userEmailAddress}
+              />
+            </div>
+            <div className="w-full">
+              <CustomLabel
+                className="font-medium text-xs font-display text-[#0d1b2a]"
+                htmlFor="email"
+              >
+                Phone number
+              </CustomLabel>
+              <CustomInputField
+                disabled
+                id="phoneNumber"
+                type="text"
+                value={
+                  profileData?.userPhoneNumber
+                    ? `${profileData?.userCountryCode}-${profileData?.userPhoneNumber}`
+                    : "N/A"
+                }
+              />
+            </div>
+            <div className="w-full">
+              <CustomLabel
+                className="font-medium text-xs font-display text-[#0d1b2a]"
+                htmlFor="email"
+              >
+                Department
+              </CustomLabel>
+              <CustomInputField
+                disabled
+                id="department"
+                type="text"
+                value={
+                  profileData?.userDepartment
+                    ? profileData?.userDepartment
+                    : "N/A"
+                }
+              />
+            </div>
+            <div className="w-full">
+              <CustomLabel
+                className="font-medium text-xs font-display text-[#0d1b2a]"
+                htmlFor="email"
+              >
+                Branch
+              </CustomLabel>
+              <CustomInputField
+                disabled
+                id="branch"
+                type="text"
+                value={
+                  profileData?.userBranch ? profileData?.userBranch : "N/A"
+                }
+              />
+            </div>
+            <div className="w-full">
+              <CustomLabel
+                className="font-medium text-xs font-display text-[#0d1b2a]"
+                htmlFor="email"
+              >
+                Faculty
+              </CustomLabel>
+              <CustomInputField
+                disabled
+                id="faculty"
+                type="text"
+                value={
+                  profileData?.userFaculty ? profileData?.userFaculty : "N/A"
+                }
+              />
+            </div>
+            <div className="w-full">
+              <CustomLabel
+                className="font-medium text-xs font-display text-[#0d1b2a]"
+                htmlFor="email"
+              >
+                Designation
+              </CustomLabel>
+              <CustomInputField
+                disabled
+                id="designation"
+                type="text"
+                value={
+                  profileData?.userRole?.toLowerCase() === "student"
+                    ? "Student"
+                    : profileData?.userDesignation
+                    ? profileData?.userDesignation
+                    : "N/A"
+                }
+              />
+            </div>
+            <div className="w-full">
+              <CustomLabel
+                className="font-medium text-xs font-display text-[#0d1b2a]"
+                htmlFor="email"
+              >
+                Joining date
+              </CustomLabel>
+              <CustomInputField
+                disabled
+                id="date"
+                type="text"
+                value={
+                  profileData?.userRole?.toLowerCase() === "student"
+                    ? profileData.userAddmissionDate
+                      ? profileData.userAddmissionDate
+                      : "N/A"
+                    : profileData?.userJoiningDate
+                    ? profileData?.userJoiningDate
+                    : "N/A"
+                }
+              />
             </div>
           </div>
         </div>
